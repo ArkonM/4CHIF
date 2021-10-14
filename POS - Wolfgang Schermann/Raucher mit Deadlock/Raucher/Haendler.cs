54c3bb37cdda3674e48e49172968a8f93c35c2a4
Armin-Schneider <58359803+Armin-Schneider@users.noreply.github.com>
2021-10-14 09:50:44 +0200

    POS

---
+++ POS - Wolfgang Schermann/Raucher mit Deadlock/Raucher/Haendler.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Raucher
+{
+    public class Haendler
+    {
+        private List<Zutaten> tisch;
+        private TextBox zutat1;
+        private TextBox zutat2;
+        private ListBox listBox;
+
+        public Haendler(TextBox eins, TextBox zwei, ListBox listBox)
+        {
+            tisch = new List<Zutaten>();
+            tisch.Add(Zutaten.Nichts);
+            tisch.Add(Zutaten.Nichts);
+            zutat1 = eins;
+            zutat2 = zwei;
+            this.listBox = listBox;
+        }
+
+        public Thread NeuerThread()
+        {
+            ThreadStart worker = new ThreadStart(Work);
+            return new Thread(worker);
+        }
+
+        private void NeueZutaten()
+        {
+            tisch.Clear();
+            int zutatNr1 = MainWindow.random.Next(3);
+            tisch.Add((Zutaten)zutatNr1);
+            int zutatNr2 = zutatNr1;
+            while (zutatNr1 == zutatNr2)
+                zutatNr2 = MainWindow.random.Next(3);
+            tisch.Add((Zutaten)zutatNr2);
+
+            String text = "" + (Zutaten)zutatNr1 + " - " + (Zutaten)zutatNr2;
+            try
+            {
+                listBox.Dispatcher.BeginInvoke(
+                  System.Windows.Threading.DispatcherPriority.Normal
+                  , new System.Windows.Threading.DispatcherOperationCallback(delegate
+                  {
+                      listBox.Items.Add(text);
+                      listBox.UpdateLayout();
+                      return null;
+                  }), null);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+            MainWindow.UpdateText(zutat1, ((Zutaten)zutatNr1).ToString(), Brushes.White);
+            MainWindow.UpdateText(zutat2, ((Zutaten)zutatNr2).ToString(), Brushes.White);
+        }
+
+        public int AnzahlZutaten()
+        {
+            return tisch[0] == Zutaten.Nichts ? 0 : 1 + tisch[1] == Zutaten.Nichts ? 0 : 1;
+        }
+
+        public Zutaten ErsteZutat()
+        {
+            return tisch[0];
+        }
+
+        public Zutaten ZweiteZutat()
+        {
+            return tisch[1];
+        }
+
+        public void NimmZutat(int zutat)
+        {
+            if(zutat == 1)
+            {
+                if (tisch[0] == Zutaten.Nichts)
+                    return;
+                tisch[0] = Zutaten.Nichts;
+                MainWindow.UpdateText(zutat1, "", Brushes.White);
+
+            }
+            else
+            {
+                tisch[1] = Zutaten.Nichts;
+                MainWindow.UpdateText(zutat2, "", Brushes.White);
+
+            }
+        }
+
+        public void NimmZutaten()
+        {
+            tisch.Clear();
+            MainWindow.UpdateText(zutat1, "", Brushes.White);
+            MainWindow.UpdateText(zutat2, "", Brushes.White);
+        }
+
+        public void Work()
+        {
+            while(true)
+            {
+                lock(this)
+                {
+                    while(AnzahlZutaten() > 0) Monitor.Wait(this);
+                    NeueZutaten();
+                    Monitor.PulseAll(this);
+                }
+            }
+        }
+    }
+}
---
+++ POS - Wolfgang Schermann/Raucher mit Deadlock/Raucher/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Raucher
+{
+    public enum Zutaten { Tabak, Papier, Streichhölzer, Nichts };
+
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Thread[] threads;
+        private KettenRaucher[] raucher;
+        static public Haendler haendler;
+        static public Random random;
+
+        public static void UpdateText(TextBox box, String text, SolidColorBrush color)
+        {
+            try
+            {
+                box.Dispatcher.BeginInvoke(
+                  System.Windows.Threading.DispatcherPriority.Normal
+                  , new System.Windows.Threading.DispatcherOperationCallback(delegate
+                  {
+                      box.Text = text;
+                      box.Background = color;
+                      box.UpdateLayout();
+                      return null;
+                  }), null);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            random = new Random();
+
+            threads = new Thread[4];
+            raucher = new KettenRaucher[3];
+            raucher[0] = new KettenRaucher(Zutaten.Papier, textBox3, "Malboro Man (P)");
+            raucher[1] = new KettenRaucher(Zutaten.Streichhölzer, textBox4, "Joe Camel (S)");
+            raucher[2] = new KettenRaucher(Zutaten.Tabak, textBox5, "Humphrey Bogart (T)");
+            haendler = new Haendler(textBox1, textBox2, listBox);
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            button1.IsEnabled = false;
+            button2.IsEnabled = true;
+
+            threads[0] = haendler.NeuerThread();
+            for(int i = 0; i < 3; i++)
+                threads[i + 1] = raucher[i].NeuerThread();
+
+            for (int i = 0; i < 4; i++)
+            {
+                threads[i].Start();
+            }
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                threads[i].Abort();
+            }
+
+            button1.IsEnabled = true;
+            button2.IsEnabled = false;
+        }
+    }
+}

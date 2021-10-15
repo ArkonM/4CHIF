013dd11ff260f9e3c80403041257895902a47204
Armin-Schneider <58359803+Armin-Schneider@users.noreply.github.com>
2021-10-15 09:47:14 +0200

    LAB-1

--- NVS - Sebastian Simon/LAB1/script.js
+++ NVS - Sebastian Simon/LAB1/script.js
@@ -1,1 +1,9 @@
 
+let carts = document.querySelectorAll('.add-cart');
+productList = [];
+sessionStorage.setItem("productList", JSON.stringify(productList));
+
+var shoppingKart = [{productID:1, quantity:0},{productID:2,qunatity:0,{productID:3,quantity:0}];
+alert(shoppingKart[1].quantity);
+shoppingKart[1].quantity += 1;
+alert(shoppingKart[1].quantity);

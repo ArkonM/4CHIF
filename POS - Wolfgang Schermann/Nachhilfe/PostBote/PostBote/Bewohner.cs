5589f5bd4e518f1b7ae6a323a29f5697f58eb708
Armin-Schneider <58359803+Armin-Schneider@users.noreply.github.com>
2021-11-02 14:29:26 +0100

    Nachhilfe

---
+++ POS - Wolfgang Schermann/Nachhilfe/PostBote/PostBote/Bewohner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace PostBote
+{
+    class Bewohner
+    {
+        public string name;
+        public Bewohner(string name)
+        {
+            this.name = name;
+        }
+
+        public void warten()
+        {
+            while (true)
+            {
+                lock (Postbote.Briefkasten)
+                {
+                    if (Postbote.post.Contains(name))    //Test ob ICH post bekommen habe
+                    {
+                        Console.WriteLine(name + " hat Post bekommen.");
+                        Postbote.post.Remove(name);     //Post wird gelöscht
+                    }
+                    else
+                    {
+                        Monitor.Wait(Postbote.Briefkasten);   //Bei Wait wird das Lock wieder freigegeben
+                    }
+                }
+            }
+        }
+
+        public void StartThread()
+        {
+            ThreadStart ts = new ThreadStart(this.warten);
+            Thread t = new Thread(ts);
+            t.IsBackground = true;
+            t.Start();
+        }
+    }
+}

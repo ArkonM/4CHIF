9ee299e5d7d768c0ae897b8a11ddc07c3e926c5e
Armin-Schneider <58359803+Armin-Schneider@users.noreply.github.com>
2022-01-16 13:12:01 +0100

    DBI 006

--- DBI_Guenter_Burgstaller/Aufgabe_006/s20170592_4C_21_006_a1_a6.sql
+++ DBI_Guenter_Burgstaller/Aufgabe_006/s20170592_4C_21_006_a1_a6.sql
@@ -35,7 +35,7 @@ select Num, SQRT(Num) "Wurzel"
 go
 
 
---Aufgabe 2
+--Aufgabe 2a
 --s20170592
 drop table if exists DecValues;
 go
@@ -104,35 +104,182 @@ end
 go
 
 
-select Val, dbo.Dec2Hex(Val) "Dex2Hex"
+select Val, dbo.Dec2Hex(Val) "Dec2Hex"
   from DecValues
 ;
 go
 
 
-
---Dec2Hex (Schleife)
+--Aufgabe 2b
 --s20170592
-drop function if exists dbo.Hex2Dec;
-go
-
-create function dbo.Hex2Dec(
-  @Hex varchar(16)
-) 
-returns varchar(16)
+create or alter function dbo.ConvertHex(
+    @Hex varchar(1)
+) returns bigint
 with schemabinding
 
 as
 
 begin
-	declare @Dec bigint;
+	if @Hex = 'A'
+		 begin
+		 return 10;
+		 end
+	else if @Hex = 'B'
+		 begin
+		 return 11;
+		 end
+	else if @Hex = 'C'
+		 begin
+		 return 12;
+		 end
+	else if @Hex = 'D'
+		 begin
+		 return 13;
+		 end
+	else if @Hex = 'E'
+		 begin
+		 return 14;
+		 end
+	else if @Hex = 'F'
+		 begin
+		 return 15;
+		 end
+	return cast(@Hex as bigint)
+end
+;
+go
 
-	  while @Hex >= 1
-	begin   
-        set @Dec = substring('0123456789ABCDEF', @Dec % 16 + 1, 1) + @Hex;
-        set @Dec = @Dec / 16;
-	end;
-      
-	return @Hex;
+
+create or alter function dbo.Hex2Dec(
+    @Hex varchar(16)
+) returns bigint
+with schemabinding
+
+as
+
+begin
+    declare @ergebnis bigint = dbo.ConvertHex(substring(@Hex, 1, 1));
+    declare @i int = 1;
+    while @i < len(@Hex)
+
+    begin
+        set @ergebnis = cast(@ergebnis as int) * 16 + dbo.ConvertHex(substring(@Hex, @i + 1, 1));
+        set @i = @i + 1;
+    end
+
+    return @ergebnis;
+end
+;
+go
+
+select Val, dbo.Dec2Hex(Val) "Hex", dbo.Hex2Dec(dbo.Dec2Hex(Val)) "Dec"
+  from DecValues
+;
+go
+
+
+--s20170592
+create or alter function dbo.DigitSumHex(
+    @Hex varchar(16)
+) returns varchar(16)
+
+as
+
+begin
+    declare @ergebnis bigint = 0;
+    declare @i int = 1;
+    while @i <= len(@Hex)
+
+    begin
+        set @ergebnis = @ergebnis + dbo.ConvertHex(substring(@Hex, @i, 1));
+        set @i = @i + 1;
+    end
+
+    return dbo.Dec2Hex(@ergebnis);
+end
+;
+go
+
+select Val, dbo.Dec2Hex(Val) "Dex2Hex", dbo.DigitSum(Val) "QuerSum", dbo.DigitSumHex(dbo.Dec2Hex(Val)) "HexQuerSum", dbo.Hex2Dec(dbo.DigitSumHex(dbo.Dec2Hex(Val))) "DecQuerSum"
+  from DecValues
+;
+go
+
+--Die verschiedenen Quersummen sind nicht übereinstimmend
+select dbo.DigitSum(255) "QuerSum", dbo.Hex2Dec(dbo.DigitSumHex(dbo.Dec2Hex(255))) "DecQuerSum"
+;
+go
+
+
+--Aufgabe 3
+--s20170592
+create or alter function dbo.ROT13(
+    @Txt varchar(30)
+) returns varchar(30)
+with schemabinding
+
+as
+
+begin
+    declare @i int = 1;
+    declare @ergebnis varchar(30) = '';
+    while @i <= len(@Txt)
+    begin
+        declare @symbol varchar(1) = substring(@Txt, @i, 1);
+        declare @lower varchar(1) = lower(@symbol);
+        declare @y varchar(1);
+		if @symbol = ' ' or @symbol = '!'
+            begin
+                set @y = @symbol;
+            end
+        else
+            if @symbol != @lower
+            begin
+				set @y = char(((ascii(@lower) - ascii('A')  + 13 ) % (ascii('Z') - ascii('A') + 1)) + ascii('A'));
+            end
+            else
+            begin
+				set @y = char(((ascii(@lower) - ascii('a')  + 13 ) % (ascii('z') - ascii('a') + 1)) + ascii('a'));
+            end
+        set @ergebnis = @ergebnis + @y;
+        set @i = @i + 1;
+    end
+    return @ergebnis;
 end;
 go
+
+
+select dbo.ROT13('Re jvyy va Frr fgrpura!') "ROT13"
+;
+go
+
+
+
+/*
+Übung 4
+s20170592
+
+Im Object Explorer muss man das man auf das Plus der Datenbank drücken, in der die Funktion enthalten ist.
+Folgend erweitert man Programmability und öffnet den Folder Funtions.
+Jetzt aufgereit sind alle Funktionen sortiert in Funktionskategorien
+
+
+Übung 5
+s20170592
+
+Weil man Nicht-Deterministische Funktionen in einer Benutzerdefinierten Funktion NICHT aufrufen kann
+RAND() und PRINT sind Nicht-Deterministisch
+
+
+Übung 6
+s20170592
+
+
+select OBJECTPROPERTY(OBJECT_ID('[dbo].[DigitSum]'), 'IsDeterministic') 
+    as "isDeterministic"
+;
+go
+
+Die Funktion muss man with schemabinding definieren
+
+*/

5c39eece2dba36d5c2d9dfde45c5563542348d27
Armin-Schneider <58359803+Armin-Schneider@users.noreply.github.com>
2022-01-27 21:22:14 +0100

    DBI_008

--- DBI_Guenter_Burgstaller/Aufgabe_008/s20170592_4C_21_008.sql
+++ DBI_Guenter_Burgstaller/Aufgabe_008/s20170592_4C_21_008.sql
@@ -48,6 +48,28 @@ select *
 go
 
 
+--Calculate February
+create or alter function dbo.FebDay(
+	@Year int
+) returns int
+as
+begin
+	if(@Year % 400 = 0)
+	begin
+		return 29;
+	end;
+	else if(@Year % 100 = 0)
+	begin
+		return 28;
+	end;
+	else if(@Year % 4 = 0)
+	begin
+		return 29;
+	end;
+	return 28;
+end;
+go
+
 --Aufgabe 9a
 --s20170592
 create or alter function dbo.MonthDaysOfYear(
@@ -64,26 +86,25 @@ begin
         insert into @DaysInMonth
             (Month, Days)
         values
-            (1, DAY(EOMONTH(CONCAT(@Year, '-', 1, '-', 1))))
-		   ,(2, DAY(EOMONTH(CONCAT(@Year, '-', 1, '-', 1))))
-		   ,(3, DAY(EOMONTH(CONCAT(@Year, '-', 1, '-', 1))))
-		   ,(4, DAY(EOMONTH(CONCAT(@Year, '-', 1, '-', 1))))
-		   ,(5, DAY(EOMONTH(CONCAT(@Year, '-', 1, '-', 1))))
-		   ,(6, DAY(EOMONTH(CONCAT(@Year, '-', 1, '-', 1))))
-		   ,(7, DAY(EOMONTH(CONCAT(@Year, '-', 1, '-', 1))))
-		   ,(8, DAY(EOMONTH(CONCAT(@Year, '-', 1, '-', 1))))
-		   ,(9, DAY(EOMONTH(CONCAT(@Year, '-', 1, '-', 1))))
-		   ,(10, DAY(EOMONTH(CONCAT(@Year, '-', 1, '-', 1))))
-		   ,(11, DAY(EOMONTH(CONCAT(@Year, '-', 1, '-', 1))))
-		   ,(12, DAY(EOMONTH(CONCAT(@Year, '-', 1, '-', 1))))
+            (1,					31)
+		   ,(2,  dbo.FebDay(@Year))
+		   ,(3,					31)
+		   ,(4,					30)
+		   ,(5,					31)
+		   ,(6,					30)
+		   ,(7,					31)
+		   ,(8,					31)
+		   ,(9,					30)
+		   ,(10,				31)
+		   ,(11,				30)
+		   ,(12,				31)
 		   ;
-
     return;
 end;
 go
 
 select * 
-  from dbo.MonthDaysOfYear(2000)
+  from dbo.MonthDaysOfYear(2001)
 ;
 go
 
@@ -104,19 +125,56 @@ begin
 
     while (@i <= 12)
         begin
-        set @s = CONCAT(@Year, '-', @i, '-', 1);
-            insert into @DaysInMonth
-                (Month, Days)
-            values
-                (@i, DAY(EOMONTH(@s)));
-            set @i = @i + 1;
+			if(@i <= 7)
+			begin
+				if(@i = 2)
+				begin
+					insert into @DaysInMonth
+						(Month, Days)
+					values
+						(@i, dbo.FebDay(@Year));
+				end;
+				else if(@i % 2 = 0)
+				begin
+				insert into @DaysInMonth
+					(Month, Days)
+				values
+					(@i, 30);
+				end;
+				else
+				begin
+				insert into @DaysInMonth
+					(Month, Days)
+				values
+					(@i, 31);
+				end;
+				set @i = @i + 1;
+			end;
+			else
+			begin
+				if(@i % 2 = 0)
+				begin
+				insert into @DaysInMonth
+					(Month, Days)
+				values
+					(@i, 31);
+				end;
+				else
+				begin
+				insert into @DaysInMonth
+					(Month, Days)
+				values
+					(@i, 30);
+				end;
+				set @i = @i + 1;
+			end;
         end;
     return;
 end;
 go
 
 
-select * 
+select *
   from dbo.MonthDaysOfYear(2000)
 ;
 go
@@ -124,24 +182,105 @@ go
 
 --Aufgabe 9c
 --s20170592
-
-
-
-
-create or alter function dbo.FebDay(
-	@Year
-) returns int
-
+create or alter function dbo.MonthDaysOfYear(
+   @Year int
+) returns @DaysInMonth table
+    ( 
+      Month tinyint,
+      Days tinyint
+    )
 as
 begin
-	if(@year % 400 == 0)
-		return 29;
-	
+    insert into @DaysInMonth
+        select *, case
+                    when Value = 1  then 31
+                    when Value = 2  then dbo.FebDay(@Year)
+                    when Value = 3  then 31
+                    when Value = 4  then 30
+                    when Value = 5  then 31
+                    when Value = 6  then 30
+                    when Value = 7  then 31
+                    when Value = 8  then 31
+                    when Value = 9  then 30
+                    when Value = 10 then 31
+                    when Value = 11 then 30
+                    when Value = 12 then 31
+                 end "Days"
+          from dbo.Range(1, 12, 1)
+    ;
+    return
 end;
 go
 
+
+select *
+  from MonthDaysOfYear(2000)
+;
+go
+
+
+--Aufgabe 9d
+--s20170592
+create or alter function dbo.MonthDaysOfYearInline(
+   @Year int
+) returns table
+as
+return
+    select *, 
+	case
+        when Value = 1  then 31
+        when Value = 2  then dbo.FebDay(@Year)
+        when Value = 3  then 31
+        when Value = 4  then 30
+        when Value = 5  then 31
+        when Value = 6  then 30
+        when Value = 7  then 31
+        when Value = 8  then 31
+        when Value = 9  then 30
+        when Value = 10 then 31
+        when Value = 11 then 30
+        when Value = 12 then 31
+        end "Days"
+    from dbo.Range(1, 13, 1)
+;
+
+go
+
+select *
+  from MonthDaysOfYearInline(2000)
+;
+go
+
+
+
 --Aufgabe 10
 --s20170592
-select * 
-  from dbo.FebDay(2000)
+select value "Year", (select Days 
+						from dbo.MonthDaysOfYear(value) 
+					   where Month=2
+					 ) "Days in February"
+  from dbo.Range(2000, 2018, 1) 
 ;
+go
+
+
+--Aufgabe 11a
+--s20170592
+select value-100 "Year", (select Days 
+						    from dbo.MonthDaysOfYear(value-100) 
+					       where Month=2
+					     ) "Days in February"
+					   , (select Days 
+						    from dbo.MonthDaysOfYear(value) 
+					       where Month=2
+					     ) "Days in February"
+					   , value "Year"
+  
+  from dbo.Range(2000, 2018, 1) 
+;
+go
+
+--Aufgabe 11b
+--s20170592
+--Weil das Jahr 2000 durch 400 dividierbar ist und somit ein Schaltjahr ist
+--1900 ist nicht durch 400 Teilbar, aber durch 100 und deswegen kein Schaltjahr

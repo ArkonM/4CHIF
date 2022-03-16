4dd10e62b103662b12f4ecb00ff8955c1682384c
Armin-Schneider <58359803+Armin-Schneider@users.noreply.github.com>
2022-03-16 11:37:34 +0100

    DBI_011

--- DBI_Guenter_Burgstaller/Aufgabe_011/s20170592_4C_21_011_a14.sql
+++ DBI_Guenter_Burgstaller/Aufgabe_011/s20170592_4C_21_011_a14.sql
@@ -1,6 +1,10 @@
 --Schneider Armin
 --s20170592
 
+use Catalog
+go
+
+
 --Aufgabe 14a)
 
 create or alter function dbo.Calendar(
@@ -18,46 +22,145 @@ create or alter function dbo.Calendar(
     )
 as
 begin
-	declare @Mo char = '';
-	declare @Tu char = '';
-	declare @We char = '';
-	declare @Th char = '';
-	declare @Fr char = '';
-	declare @Sa char = '';
-	declare @SU char = '';
+	declare @Mo char(2) = '';
+	declare @Tu char(2) = '';
+	declare @We char(2) = '';
+	declare @Th char(2) = '';
+	declare @Fr char(2) = '';
+	declare @Sa char(2) = '';
+	declare @Su char(2) = '';
 
 	declare @i int;
-	declare @y int = DATEPART(WEEKDAY, 01-@Month-@Year);
+	declare @weekday int = DATEPART(WEEKDAY, DATEFROMPARTS(@Year, @Month, 1));
 	
 	set @i = case
-		when @y = 1 then 1
-		when @y = 2 then 0
-		when @y = 3 then -1
-		when @y = 4 then -2
-		when @y = 5 then -3
-		when @y = 6 then -4
-		when @y = 7 then -5
+		when @weekday = 2 then  1
+		when @weekday = 3 then  0
+		when @weekday = 4 then -1
+		when @weekday = 5 then -2
+		when @weekday = 6 then -3
+		when @weekday = 7 then -4
+		when @weekday = 1 then -5
 	end;
 
-	while(@i < dbo.DaysOfMonthOfYear(@Month, @Year))
+	while(@i <= dbo.DaysOfMonthOfYear(@Year,@Month))
 	begin
-		
-	end;
 
-	insert into @Calendar
+		if(@i > 0 and @i <= dbo.DaysOfMonthOfYear(@Year,@Month))
+		begin
+		set @Mo = @i
+		end;
+
+		else
+		begin
+		set @Mo = ''
+		end;
+		set @i = @i+1
+
+
+		if(@i > 0 and @i <= dbo.DaysOfMonthOfYear(@Year,@Month))
+		begin
+		set @Tu = @i
+		end;
+
+		else
+		begin
+		set @Tu = ''
+		end;
+		set @i = @i+1
+
+
+		if(@i > 0 and @i <= dbo.DaysOfMonthOfYear(@Year,@Month))
+		begin
+		set @We = @i
+		end;
+
+		else
+		begin
+		set @We = ''
+		end;
+		set @i = @i+1
+
+
+		if(@i > 0 and @i <= dbo.DaysOfMonthOfYear(@Year,@Month))
+		begin
+		set @Th = @i
+		end;
+
+		else
+		begin
+		set @Th = ''
+		end;
+		set @i = @i+1
+
+
+		if(@i > 0 and @i <= dbo.DaysOfMonthOfYear(@Year,@Month))
+		begin
+		set @Fr = @i
+		end;
+
+		else
+		begin
+		set @Fr = ''
+		end;
+		set @i = @i+1
+
+
+		if(@i > 0 and @i <= dbo.DaysOfMonthOfYear(@Year,@Month))
+		begin
+		set @Sa = @i
+		end;
+
+		else
+		begin
+		set @Sa = ''
+		end;
+		set @i = @i+1
+
+
+		if(@i > 0 and @i <= dbo.DaysOfMonthOfYear(@Year,@Month))
+		begin
+		set @Su = @i
+		end;
+
+		else
+		begin
+		set @Su = ''
+		end;
+		set @i = @i+1
+
+
+		insert into @Calendar
 		( Mo,  Tu,  We,  Th,  Fr,  Sa,  Su)
-	values
+		values
 		(@Mo, @Tu, @We, @Th, @Fr, @Sa, @Su)
+
+
+	end;
 
 	return;
 end;
 go
+
+--s20170592
+select *
+ from dbo.Calendar(2022, 2)
+;
+go
 	
 
+declare @lauf int = 0;
 
+while(@lauf < 12)
+begin
 
+	set @lauf = @lauf+1;
 
+	select DATENAME(MONTH, DATEFROMPARTS(2022, @lauf, 1))
+	;
 
-select *
-  from dbo.Calendar(2011, 2);
-go
+	select *
+		from dbo.Calendar(2022, @lauf)
+	;
+
+end;
